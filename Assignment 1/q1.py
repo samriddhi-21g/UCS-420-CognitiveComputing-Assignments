f7a7427145c7907276ccdac785040aa7896854b8
samriddhi-21g <samriddhi.21g@gmail.com>
2026-08-10 01:26:17 +0530

    Initial Commit

---
+++ Assignment 1/q1.py
@@ -0,0 +1,66 @@
+# Online Python compiler (interpreter) to run Python online.
+# Writ""e Python 3 code in this online editor and run it.
+print("Hello World!")
+
+print("Samriddhi")
+print("Samriddhi")
+print("Samriddhi")
+
+x = 10
+y = 20
+z = x+y
+print(x,"+",y,"=",z)
+
+string1 = "Hello"
+string2 = "World"
+print(string1 + " " + string2)
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+c = a+b
+print(a, "+", b, "=", c)
+
+for i in range(1,11):
+     print(i)
+     
+i = 1
+while i<=10:
+    print("5 x",i, "= ", 5*i)
+    i = i+1
+    
+n = int(input("Enter number (for table) : "))
+i = 1
+while i<=10:
+    print(n,"x",i, "= ", n*i)
+    i = i+1
+
+n2= int(input("Enter number (for sum) : "))
+sum = 0
+for i in range(n2+1):
+    sum = sum+i
+print("Sum till n : " ,sum)
+
+if a>b:
+    print("a>b")
+else:
+    print("a<b")
+
+if a%2:
+    print("Number is odd")
+else:
+    print("Number is even")
+
+i = 2
+n3 = int(input("Enter a number for prime number : "))
+for i in range(n3):
+    if n3%i=0 :
+        print(n3, " is a prime number.")
+        break
+    i=i+1
+
+if a==b:
+    print("a=b")
+elf a>b:
+    print("a>b")
+else:
+    print("a<b")
